85f5cd6e86d66581e908866c22d27cce23af2822
paulina <paulinafs89@gmail.com>
2019-10-16 11:51:58 -0300

    desafio form

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,4 +1,28 @@
 class SalesController < ApplicationController
+
   def new
+  end
+  def create
+    sale = Sale.new(sales_params)
+    sale.total = sale.value.to_f * (1-(sale.discount.to_f/100))
+    if sale.tax == 0
+      sale.total = sale.total * 1.19
+      sale.tax = 19
+    else
+      sale.tax = 0
+    end
+    sale.save
+    if sale.save
+      redirect_to sales_done_path
+    end
+  end
+
+  def done
+    @sales = Sale.all
+  end
+
+  private
+  def sales_params
+    params.require(:sale).permit(:cod, :detail, :category, :value, :discount, :tax)
   end
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,7 @@
 class Sale < ApplicationRecord
+  validates :cod, uniqueness: true
+  validates :detail, presence: true
+  validates_inclusion_of :category, :in => [1, 2, 3, 4 ,5]
+  validates:value, numericality: { only_integer: true, grater_than_or_equal_to: 0, message: "%{value} is not a valid size" }
+  validates :discount, numericality: { only_integer: true, grater_than_or_equal_to: 0, less_than_or_equal_to: 40, message: "%{value} is not a valid size" }
 end
